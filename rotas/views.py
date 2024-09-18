74b4e003eb5192fec86f2109646753c27e739b5f
GuRuffini <ruffinigustavo@gmail.com>
2024-09-18 02:20:43 -0300

    filtro de ativo configurado na views.py

--- rotas/views.py
+++ rotas/views.py
@@ -4,14 +4,14 @@ from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 class AmbienteViewSet(viewsets.ModelViewSet):
-    queryset = Ambiente.objects.all()
+    queryset = Ambiente.objects.filter(ativo=True)
     serializer_class = AmbienteSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     filterset_fields = ['ativo',]
     search_fields = ['ambiente', 'bloco', 'andar']
 
 class RotaViewSet(viewsets.ModelViewSet):
-    queryset = Rota.objects.all()
+    queryset = Rota.objects.filter(ativo=True)
     serializer_class = RotaSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     filterset_fields = ['ativo',]
