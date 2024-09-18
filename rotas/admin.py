14160d157e5d5c9e2903f1a8ae4da6a627b4fab2
GuRuffini <ruffinigustavo@gmail.com>
2024-09-18 01:48:39 -0300

    admin.py configurado

--- rotas/admin.py
+++ rotas/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from rotas.models import Ambiente, Rota
 
-# Register your models here.
+
+class Ambientes(admin.ModelAdmin):
+    list_display = ('id', 'ambiente', 'adicionado', 'ativo')
+    list_display_links = ('id', 'ambiente')
+    list_per_page = 10
+    search_fields = ('ambiente',)
+    ordering = ('id',)
+
+admin.site.register(Ambiente, Ambientes)
+
+class Rotas(admin.ModelAdmin):
+    list_display = ('id', 'origem', 'destino', 'adicionado', 'ativo')
+    list_display_links = ('id',)
+    list_per_page = 10
+    search_fields = ('id',)
+    ordering = ('id',)
+
+admin.site.register(Rota, Rotas)
